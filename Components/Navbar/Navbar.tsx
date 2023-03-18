0ff6d6fe74f7bbf96aa9524c70f8145731932779
WaghAshu03 <waghashutosh03@gmail.com>
2023-03-18 15:14:55 +0530

    Initial Commit

---
+++ Components/Navbar/Navbar.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import NavbarStyle from "./Navbar.module.scss";
+
+const Navbar = () => {
+  return <nav className={NavbarStyle.Navbar}>Navbar</nav>;
+};
+
+export default Navbar;
